debe052e5beed45d59afcc16ad15f8baf66730da
N0RBI475 <norrrrbi.475@gmail.com>
2025-02-13 02:53:24 +0300

    Str2

---
+++ src/Main/Str2your/Str2.jsx
@@ -0,0 +1,14 @@
+
+import Section1 from './section1/section1'
+import Section2 from './section2/section2'
+
+export default function Str2(){
+    return(
+        <>
+            <main>
+                <Section1/>
+                <Section2/>
+            </main>
+        </>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.scss';
+import Header from './Header/header';
+import { Route, Routes, HashRouter } from 'react-router-dom'; // Import HashRouter correctly
+import Footer from './Footer/Footer';
+import Str1 from './Main/Str1my/Str1';
+import Str2 from './Main/Str2your/Str2';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <HashRouter>
+      <Header />
+      <Routes>
+        <Route path="/Str1" element={<Str1 />} /> 
+        <Route path="/Str2" element={<Str2 />} /> 
+      </Routes>
+      <Footer />
+    </HashRouter>
+  </StrictMode>
+);
